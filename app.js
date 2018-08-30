2e55b3b3dfe2c7012846ce4dc2c558b781a90558
Peter T. Herr <petertherr@gmail.com>
2018-08-30 08:44:29 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+var userScore = $('#userTeamScore');
+userScore.value = 0;
+var computerScore = $('#computerTeamScore')
+computerScore.value = 0;
+
+
+
+$('#rock').click(function(){
+	var opponent = getOpponentChoice();
+	if (opponent =='paper')
+		youLose('rock', opponent);
+	else if (opponent == 'scissors')
+		youWin('rock', opponent);
+	else
+		youTie('rock', opponent);s
+
+	
+	
+});
+
+$('#paper').click(function(){
+	var opponent = getOpponentChoice();
+	if (opponent =='scissors')
+		youLose('paper', opponent);
+	else if (opponent == 'rock')
+		youWin('paper', opponent);
+	else
+		youTie('paper', opponent);
+});
+
+$('#scissors').click(function(){
+	var opponent = getOpponentChoice();
+	if (opponent =='rock')
+		youLose('scissors', opponent);
+	else if (opponnet == 'paper')
+		youWin('scissors',	opponent);
+	else
+		youTie('scissors',opponent);
+});
+
+
+function youWin(yourHand, opponentHand){
+	userScore.html(userScore.value+=1)
+	$('#result').html("You win" + "<div>"+yourHand.toUpperCase()+" beats "+opponentHand.toUpperCase()+".</div>")
+}
+
+function youLose(yourHand, opponentHand){
+	computerScore.html(computerScore.value+=1)
+	$('#result').html("You lose" + "<div>"+yourHand.toUpperCase()+" loses to "+opponentHand.toUpperCase()+".</div>")
+}
+
+function youTie(yourHand, opponentHand){
+	$('#result').html("TIE!")
+}
+
+//method...
+function getOpponentChoice(){
+	var randomNumber = Math.round((Math.random()*10)%3);
+	var opponent;
+
+	//switch statement
+	
+	switch(randomNumber) {
+		case 0:
+			opponent = "rock";
+			break;
+		case 1:
+			opponent = "paper";
+			break;
+		case 2:
+			opponent = "scissors";
+			break;
+		default:
+			opponent = "rock";
+	}
+
+	return opponent;
+
+}
